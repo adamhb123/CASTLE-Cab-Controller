7f4a82bd4aed9d919645141990c1c499bd3b8b12
Adam Brewer <adamhb321@gmail.com>
2026-04-06 18:42:32 -0400

    Add buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,49 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-const int BTN_START_2 = 5; // D0 (see PINOUT.md)
-
-template <typename TState>
-class Button {
-  private:
-    int pin;
-  public:
-    TState lastState;
-    Button(int pin, TState defaultState){
-      this->pin = pin;
-      this->lastState = defaultState;
-    }
-    void setPinMode(const int btnPinMode){
-      pinMode(this->pin, btnPinMode);
-    }
-    TState read(){
-      return digitalRead(this->pin);
-    }
+struct Button
+{
+  const char *name;
+  int pin;
+  int lastState;
+  Button(const char *name, int pin) : name(name), pin(pin), lastState(HIGH) {}
 };
 
-// Define buttons
-Button<int> btnStart2 = Button<int>(5, HIGH);
+Button buttons[] = {
+  Button("Start1", 5),
+  Button("Start2", 6),
+  Button("LIVE", 7),
+  Button("JoyUp", 8), // Yellow wires
+  Button("JoyDown", 9), // Black wires
+  Button("JoyRight", 10), // Red Wires
+  Button("JoyLeft", 11), // Green Wires
+  Button("Fire", 12),
+  Button("Aux1", 13),
+  Button("Aux2", 14),
 
+};
 
-void setup() {
+const size_t n_buttons = sizeof(buttons) / sizeof(buttons[0]);
+
+void setup()
+{
   Serial.begin(115200);
-  btnStart2.setPinMode(INPUT_PULLUP);
-}
-
-void loop() {
-  if(btnStart2.read() == LOW && btnStart2.lastState == HIGH){
-    Serial.println("BTN_START_2 pressed");
-    delay(100);
+  for (size_t i = 0; i < n_buttons; i++)
+  {
+    pinMode(buttons[i].pin, INPUT_PULLUP);
   }
 }
-  // put your main code here, to run repeatedly:
 
-// put function definitions here:
-//int myFunction(int x, int y) {
-//  return x + y;
-//}
+void loop()
+{
+  for (size_t i = 0; i < n_buttons; i++)
+  {
+    int state = digitalRead(buttons[i].pin);
+    if (state == LOW && buttons[i].lastState == HIGH)
+    {
+      char buff[32];
+      sprintf(buff, "//%s pressed", buttons[i].name);
+      Keyboard.println(buff);
+    }
+    buttons[i].lastState = state;
+  }
+}
